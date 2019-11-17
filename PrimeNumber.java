30fce3a691257b92b05cfb3754d6e03fc0536d77
Jhansi Bendi <jhansi_bendi@gap.com>
2019-11-17 14:01:50 -0800

    to find if a number is prime or not

---
+++ PrimeNumber.java
@@ -0,0 +1,28 @@
+
+public class PrimeNumber {
+	
+	public static boolean isPrime(int n)
+	{
+		for (int i = 2; i*i <= n; i++)
+		{
+			if(n%i==0)
+			{
+				return false;
+			}
+		}
+		return true;
+	}
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num = 139;
+		//boolean prime = isPrime(num);
+		//System.out.println(prime);
+		if(isPrime(num) )
+			System.out.println(num + " is prime");
+		else
+			System.out.println(num + " is not prime");
+
+	}
+
+}
