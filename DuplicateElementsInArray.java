53bec64ad747059f9b6294b7e650a5286b257782
Ramkilli <rkilli@scu.edu>
2026-01-08 15:46:53 -0800

    commit

---
+++ DuplicateElementsInArray.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class DuplicateElementsInArray {
+    public static void main(String[] args) {
+        int [] arr = {2,3,4,5,6,2,4,7,8};
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for (int i = 0 ;i<arr.length; i++)
+        {
+            if(map.containsKey(arr[i]))
+            {
+                map.put(arr[i],map.get(arr[i])+1);
+            }
+            else
+            {
+                map.put(arr[i],1);
+            }
+        }
+        for(Map.Entry<Integer,Integer> entry : map.entrySet())
+        {
+            System.out.println(entry.getKey() +":"+ entry.getValue());
+        }
+    }
+}
